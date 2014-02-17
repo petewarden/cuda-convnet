47b59cb11a4ea0cf89e84cf858a5437afd2d1039
Pete Warden <pete@jetpac.com>
2014-02-17 15:20:23 -0800

    Debug instrumentation for single test

--- gpumodel.py
+++ gpumodel.py
@@ -251,7 +251,7 @@ class IGPUModel:
         num_classes = self.test_data_provider.get_num_classes()
         softmax_idx = self.get_layer_idx('probs', check_type='softmax')
         NUM_IMGS = 1
-        NUM_TOP_CLASSES = min(num_classes, 4) # show this many top labels
+        NUM_TOP_CLASSES = min(num_classes, 5) # show this many top labels
         label_names = self.test_data_provider.batch_meta['label_names']
         preds = n.zeros((NUM_IMGS, num_classes), dtype=n.single)
         rand_idx = 0
